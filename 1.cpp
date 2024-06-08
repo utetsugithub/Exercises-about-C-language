43df254a59ce805baca0c8b3d7c169c1ac8f3dde
utetsugithub <z10209430522@gmail.com>
2024-06-08 14:51:45 +0900

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -4,3 +4,10 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+
+#include <iostream>
+        int main(void){
+                std::cout<<"Tokushima University Faculty of Engineering Intelligent Informatics Course\n";
+                return 0;
+        }
