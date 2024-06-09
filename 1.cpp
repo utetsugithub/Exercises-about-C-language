26494192e605f61e72632c77f028ccd59c5c6ee9
utetsugithub <z10209430522@gmail.com>
2024-06-09 17:07:04 +0900

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -14,3 +14,25 @@ int main(void){
                 std::cout<< b << "月" << a << "日\n";
                 return 0;
         }
+
+
+#include <iostream>
+
+int main(void){
+        int a = 25;
+        int b = 4;
+
+        int add = a + b;
+        int sub = a - b;
+        int mlt = a * b;
+        int div = a / b;
+        int dpl = a % b;
+
+        std::cout<< "a + b = "<< add << "\n"
+                 << "a - b = "<< sub << "\n"
+                 << "a * b = "<< mlt << "\n"
+                 << "a / b = "<< div << "\n"
+                 << "a % b = "<< dpl << "\n";
+
+                return 0;
+        }
