457d3f55263a5d8517ac0043fb899bb4fd9c08e3
utetsugithub <z10209430522@gmail.com>
2024-06-12 12:38:22 +0900

    Update 2.c

--- 2.c
+++ 2.c
@@ -113,3 +113,225 @@ int main(void){
         printf("ave(9, 6) = %f\nave(56, 8) = %f\n", i, j);
         return 0;
 }
+
+
+
+/*24 4つの整数を入力し，その平均値を実数で表示せよ*/
+#include <stdio.h>
+
+int main(void){
+    int a[4], sum = 0; 
+    for(int i = 0; i <= 3; i++){
+        printf("%d個目の整数: \n", i+1);
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+    printf("平均は%f", sum / (double)4);
+    return 0;
+}
+
+
+
+/*25 入力された２つの数字a,bを，a月b日と考え，１年の何日目かを表示せよただしaは1〜3の値とし，うるう年は考えない（２月は２８日までとする）*/
+#include <stdio.h>
+	
+ int main(void){
+  int a = 0, b = 0;
+   printf("please put month and day:\n");
+    scanf("%d %d", &a, &b);
+     printf("%d月%d日\n", a, b);
+    
+     switch(a){
+      case 1:
+       printf("%d日目", b);
+       break;
+        case 2:
+         printf("%d日目", 31+b);
+	 break;
+	  case 3:
+           printf("%d日目", 59+b);
+	   break;
+	    case 4:
+             printf("%d日目", 90+b);
+	     break;
+              case 5:
+               printf("%d日目", 120+b);
+	       break;
+                case 6:
+                 printf("%d日目", 151+b);
+		 break;
+       case 7:
+       printf("%d日目", 181+b);
+       break;
+        case 8:
+         printf("%d日目", 212+b);
+         break;
+          case 9:
+           printf("%d日目", 243+b);
+           break;
+            case 10:
+             printf("%d日目", 273+b);
+             break;
+              case 11:
+               printf("%d日目", 304+b);
+               break;
+                case 12:
+                 printf("%d日目", 334+b);
+                 break;
+
+
+     } 
+     return 0;	
+	}
+
+
+
+/*26 if文でなくswitchを用いて書き換えよ*/
+#include <stdio.h>
+
+int main(void)
+{
+int month;
+
+printf("何月ですか：");
+ scanf("%d", &month);
+
+ switch (month){
+  case 3:
+   case 4:
+    case 5:
+     printf("%d月は春です。\n", month);
+      break;
+       case 6:
+        case 7:
+         case 8:
+          printf("%d月は夏です。\n", month);
+           break;
+            case 9:
+             case 10:
+              case 11:
+               printf("%d月は秋です。\n", month);
+                break;
+                 case 1:
+                  case 2:
+                   case 12:
+                    printf("%d月は冬です。\n", month);
+                     break;
+                　　　default:
+                　　　　printf("%d月はありませんよ!!\a\n", month);
+                        　break;
+                        　　　　}
+        return 0;
+}
+
+
+
+/*27 整数値を読み込んで，その数が10で割り切れるかどうかを表示するプログラムを作成せよ．なお，表示後にもう一度続けるかどうか確認し，それに応じて何度でも好きなだけ繰り返せるようにせよ．*/
+#include <stdio.h>
+ int main(void){
+ 　int j;
+ 　　while(j){
+ 　　　int number;
+ 　　　int repition;
+ 　　　int choice;
+ 　　　　printf("数字を入力してください: ");
+ 　　　　scanf("%d", &number);
+ 　　　　　if(number % 10 == 0){
+ 　　　　　　printf("10で割り切れる\n");
+ 　　　　　}
+ 　　　　　　else{
+ 　　　　　　　printf("10で割り切れない\n");
+ 　　　　　　}
+ 　　　　　　　printf("もう一度やりますか(Yes/1,No/0)\n");
+         　　　scanf("%d", &choice);
+ 　　　　　　　　if(choice == 1){
+	 　　　　　　　j = 1;
+ 　　　　　　　　}
+ 　　　　　　　　　else{
+ 　　　　　　　　　　　j = 0;
+ 　　　　　　　　　　　}
+　　　　　}
+ 　　return 0;
+ }
+
+
+
+/*28 二つの整数値を読み込んで，小さい方の数以上で大きい方の数以下の全整数を加えた値を表示するプログラムを作成せよ.*/
+#include <stdio.h>
+ int main(void){
+  int a, b;
+  int sum = 0;
+   printf("整数値a:");
+   scanf("%d", &a);
+   printf("整数値b:");
+   scanf("%d", &b);
+    int min = a < b ? a : b;
+    int max = a > b ? a : b;
+     int i = min;
+      do{
+	sum += i;
+	i++;
+        }while(i <= max);
+	 printf("%d以上%d以下の総計は%d", min, max, sum);
+return 0;
+}
+
+
+
+/*29 について、繰り返すかどうかを尋ねるのではなくて，読み込んだ整数値が偶数だったら繰り返すようにせよ*/
+#include <stdio.h>
+
+int main(void)
+
+{
+    int nu;
+    int retry;
+
+        do {
+
+        printf("整数を入力せよ。：");
+        scanf("%d", &nu);
+
+        if (nu == 0)
+            puts("その数は０です。");
+        else if (nu > 0)
+            puts("その数は正です。");
+        else
+            puts("その数は負です。");
+
+        if(nu % 2 != 0){
+                retry = 1;
+        }
+        else{
+               retry = 0;
+        }
+
+    } while (!(retry != 0));
+
+    return 0;
+}
+
+
+
+//30 0が読み込まれるまで，複数の整数値を読み込み，その中の最大値を表示するプログラムを作成せよ
+  #include <stdio.h>
+
+   int main(void){
+       int n = 1;
+       int i;
+       int max = 0;
+
+       while(n){
+	   printf("整数を入れてください:");
+	   scanf("%d", &i);
+         if (max < i){
+          max = i;
+	}   
+	   if (i == 0){
+	       n = 0;
+         }   
+	  }
+	 
+	 printf("最大の数字は%d\n", max);
+   return 0;
+   }
