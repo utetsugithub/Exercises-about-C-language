0657c28de8b7aa234fdd992ff570867b335f5571
utetsugithub <z10209430522@gmail.com>
2024-06-18 16:23:54 +0900

    Update 1.c

--- 1.c
+++ 1.c
@@ -280,3 +280,232 @@ printNth(n);
 return 0;
 }
 
+
+/*19 整数引数 a と b を取り，その和 (a + b) を返す関数を，int add(int a, int b) と>して設計し，その動作を確認できる main() と共にプログラムを作成しなさい*/
+
+#include <stdio.h>
+
+int add(int a, int b);
+
+int add(int a, int b){
+        int m = 0;
+        m = a + b;
+        return m;
+}
+
+int main(void){
+        int i, j = 0;
+
+        i = add(3, 4);
+        j = add(6, 88);
+
+        printf("a(3, 4) = %d\na(6, 88) = %d\n", i, j);
+        return 0;
+}
+
+
+/*20 整数引数 a と b を取り，その差 (a − b) を返す関数を，int sub(int a, int b) と>して設計し，その動作を確認できる main() と共にプログラムを作成しなさい*/
+
+#include <stdio.h>
+
+int sub(int a, int b);
+
+int sub(int a, int b){
+        int m = 0;
+        m = a - b;
+        return m;
+}
+
+int main(void){
+        int i, j = 0;
+
+        i = sub(3, 4);
+        j = sub(6, 88);
+
+        printf("a(3, 4) = %d\na(6, 88) = %d\n", i, j);
+        return 0;
+}
+
+
+/*21 整数引数 m を取り，m の自乗 (二乗) を返す関数を，int square(int m) として設計>し，その動作を確認できる main() と共にプログラムを作成しなさい．*/
+
+#include <stdio.h>
+
+int square(int a);
+
+int square(int a){
+        int m = 0;
+        m = a * a;
+        return m;
+}
+
+int main(void){
+
+        int i, j = 0;
+
+        i = square(3);
+        j = square(6);
+
+        printf("square(3) = %d\nsquare(6) = %d\n", i, j);
+        return 0;
+}
+
+/*22 整数引数 a と b を取り，その商 (a ÷ b) を返す関数を，double div(int a, int b) として設計し，その動作を確認できる main() と共にプログラムを作成しなさい*/
+
+#include <stdio.h>
+
+double div(int a, int b);
+
+double div(int a, int b){
+        double m = 0;
+        m = a / (double)b;
+        return m;
+}
+
+int main(void){
+
+        double i, j = 0;
+
+        i = div(9, 6);
+        j = div(56, 9);
+
+        printf("div(9, 6) = %f\ndiv(56, 8) = %f\n", i, j);
+        return 0;
+}
+
+
+/*23 倍精度浮動小数点数引数 a と b を取り，その商 (a÷b) を返す関数を，double div(double a, double b)として設計し，その動作を確認できる main() と共にプログラムを作>成しなさい．*/
+
+#include <stdio.h>
+
+double ave(int a, int b);
+
+double ave(int a, int b){
+        double m = 0;
+        m = (a + b) / (double)2;
+        return m;
+}
+
+int main(void){
+
+        double i, j = 0;
+
+        i = ave(9, 6);
+        j = ave(56, 9);
+
+        printf("ave(9, 6) = %f\nave(56, 8) = %f\n", i, j);
+        return 0;
+}
+
+
+
+/*24 4つの整数を入力し，その平均値を実数で表示せよ*/
+
+#include <stdio.h>
+
+int main(void){
+    int a[4], sum = 0; 
+    for(int i = 0; i <= 3; i++){
+        printf("%d個目の整数: \n", i+1);
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+    printf("平均は%f", sum / (double)4);
+    return 0;
+}
+
+
+
+/*25 入力された２つの数字a,bを，a月b日と考え，１年の何日目かを表示せよただしaは1〜3の値とし，うるう年は考えない（２月は２８日までとする）*/
+
+#include <stdio.h>
+	
+ int main(void){
+  int a = 0, b = 0;
+   printf("please put month and day:\n");
+    scanf("%d %d", &a, &b);
+     printf("%d月%d日\n", a, b);
+    
+     switch(a){
+      case 1:
+       printf("%d日目", b);
+       break;
+        case 2:
+         printf("%d日目", 31+b);
+	 break;
+	  case 3:
+           printf("%d日目", 59+b);
+	   break;
+	    case 4:
+             printf("%d日目", 90+b);
+	     break;
+              case 5:
+               printf("%d日目", 120+b);
+	       break;
+                case 6:
+                 printf("%d日目", 151+b);
+		 break;
+       case 7:
+       printf("%d日目", 181+b);
+       break;
+        case 8:
+         printf("%d日目", 212+b);
+         break;
+          case 9:
+           printf("%d日目", 243+b);
+           break;
+            case 10:
+             printf("%d日目", 273+b);
+             break;
+              case 11:
+               printf("%d日目", 304+b);
+               break;
+                case 12:
+                 printf("%d日目", 334+b);
+                 break;
+
+
+     } 
+     return 0;	
+	}
+
+
+
+/*26 if文でなくswitchを用いて書き換えよ*/
+
+#include <stdio.h>
+
+int main(void)
+{
+int month;
+
+printf("何月ですか：");
+ scanf("%d", &month);
+
+ switch (month){
+  case 3:
+   case 4:
+    case 5:
+     printf("%d月は春です。\n", month);
+      break;
+       case 6:
+        case 7:
+         case 8:
+          printf("%d月は夏です。\n", month);
+           break;
+            case 9:
+             case 10:
+              case 11:
+               printf("%d月は秋です。\n", month);
+                break;
+                 case 1:
+                  case 2:
+                   case 12:
+                    printf("%d月は冬です。\n", month);
+                     break;
+                　　　default:
+                　　　　printf("%d月はありませんよ!!\a\n", month);
+                        　break;
+                        　　　　}
+        return 0;
+}
